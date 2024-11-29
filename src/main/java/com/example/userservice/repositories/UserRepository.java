fa7eacbc6386df93e5a5a7d1065b5a8ce91051f2
saurabh989759 <saurabh98king@gmail.com>
2024-11-30 00:02:27 +0530

    new changes

--- src/main/java/com/example/userservice/repositories/UserRepository.java
+++ src/main/java/com/example/userservice/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.example.userservice.repositories;
 
-public class UserRepository {
+import com.example.userservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
