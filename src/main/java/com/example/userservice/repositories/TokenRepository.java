d59d0e9604b9f138343d32b57137ba02f67864cd
saurabh989759 <saurabh98king@gmail.com>
2024-12-03 23:48:03 +0530

    nn

--- src/main/java/com/example/userservice/repositories/TokenRepository.java
+++ src/main/java/com/example/userservice/repositories/TokenRepository.java
@@ -2,16 +2,22 @@ package com.example.userservice.repositories;
 
 import com.example.userservice.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     @Override
-    Token save(Token token);
+    Token save(Token  token);
+
 
 
     //Declared Query
     Optional<Token> findByValueAndDeletedAndExpiryAtGreaterThan(String value, boolean deleted, Date expiryAt);
 
+
+    @Query("SELECT COUNT(t) FROM tokens t WHERE t.user.id = :userId AND t.deleted = false")
+    long countByUserId(@Param("userId") Long userId);
 }
